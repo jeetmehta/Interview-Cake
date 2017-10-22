14633e52e863e37f9d506ecfc4006b0e29ac8d97
Jeet Mehta <jeet25.m@gmail.com>
2017-10-22 19:52:20 -0400

    Optimized for O(n) time and O(n) space - still using 3 new vectors though, can be reduced

--- product-of-other-numbers.cpp
+++ product-of-other-numbers.cpp
@@ -14,18 +14,32 @@ vector <int> getProductsOfAllIntsExceptAtIndex(vector <int> input)
 {
 	if (input.size() <= 1)
 		return input;
-	
-	vector <int> output(input.size());
+
+	// Products from left side
+	int leftRunningProduct = 1;
+	vector <int> leftProducts(input.size());
 	for (int i = 0; i < input.size(); i++)
 	{
-		int product = 1;
-		for (int j = 0; j < input.size(); j++)
-		{
-			if (i != j)
-				product *= input[j];
-		}
-		output[i] = product;
+		leftProducts[i] = leftRunningProduct;
+		leftRunningProduct *= input[i];
 	}
+
+	// Products from right side
+	int rightRunningProduct = 1;
+	vector <int> rightProducts(input.size());
+	for (int i = input.size() - 1; i >= 0; i--)
+	{
+		rightProducts[i] = rightRunningProduct;
+		rightRunningProduct *= input[i];
+	}
+
+	// Combine to get final result
+	vector <int> output;
+	for (int i = 0; i < input.size(); i++)
+	{
+		output.push_back(leftProducts[i] * rightProducts[i]);
+	}
+
 	return output;
 }
 
